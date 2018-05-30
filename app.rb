f71b8d43f6cdf11b454c307869dab5726f44b172
Daniel Ridgway <ridginald@gmail.com>
2018-05-30 11:42:50 +0100

    using params

--- app.rb
+++ app.rb
@@ -11,7 +11,13 @@ get '/secret' do
   "This is a secret message!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
