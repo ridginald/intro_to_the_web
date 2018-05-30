611a8245e68897300ab5172794450953057699bf
Daniel Ridgway <ridginald@gmail.com>
2018-05-30 10:42:20 +0100

    a cat

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+  "This is a secret message!"
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
